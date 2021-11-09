2c7b64d77ce0358ce7246bd7f5808f8c28faf44d
HerculesVE <79154092+HerculesVE@users.noreply.github.com>
2021-11-09 18:00:11 -0300

    tabela do cliente comentada

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -9,4 +9,13 @@ use Illuminate\Database\Schema\Blueprint;
 class Cliente extends Model
 {
     use HasFactory;
+    /*public function up(){
+        Schema::create('cliente',function(Blueprint Stable){
+            Stable->increments('id');
+            Stable->string('nome',50);
+            Stable ->string("endereço",80)->nullable();
+            Stable ->string("email")->nullable();
+            Stable ->string("telefone")->nullable();
+        });
+    }*/
 }
